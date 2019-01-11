fa217efcef2f3cc8ac633695fc1e509e6b1628a3
karthickthidiff <karthick@thidiff.com>
2019-01-12 01:16:58 +0530

    Added Voting poll create and list screens

--- src/App.js
+++ src/App.js
@@ -3,85 +3,106 @@ import { Container, Row, Col, Card, CardBody, Button, Form, FormGroup, Label, In
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-	      <Container>
-		      <Row>
-			      <Col className="p-3" sm="12">
-				      <h2>VOTING APP</h2>
-			      </Col>
-			      <Col sm={{ size: 8, order: 2, offset: 1 }}>
-				      <Card className="p-2">
-					      <CardBody className="text-left">
-						      <h3>Make a new POLL:</h3>
-						      <Col sm={{ size: 8 }}>
-							      <Form>
-								      <FormGroup>
-									      <Label for="pollTitle">Title:</Label>
-									      <Input type="text" name="pollTitle" id="pollTitle" placeholder="Poll title" />
-								      </FormGroup>
+	constructor(props) {
+		super(props);
+		this.state = {
+			votingPolls: localStorage.getItem("votingPolls") ? JSON.parse(localStorage.getItem("votingPolls")) : []
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-								      <FormGroup>
-									      <Label for="pollOptions">Options</Label>
-									      <Input type="select" name="pollOptions" id="pollOptions" placeholder="Select">
-										      <option>Select</option>
-										      <option>1</option>
-										      <option>2</option>
-										      <option>3</option>
-										      <option>4</option>
-										      <option>5</option>
-									      </Input>
-								      </FormGroup>
-								      <Button color="primary">Create</Button>
-							      </Form>
-						      </Col>
-					      </CardBody>
-				      </Card>
-			      </Col>
+	handleChange(e) {
+	    this.setState({[e.target.name]: e.target.value});
+    }
 
-			      <Col className="mt-3" sm={{ size: 8, order: 2, offset: 1 }}>
-				      <Card className="p-2">
-					      <CardBody className="text-left">
-						      <h3>Polls List:</h3>
-						      <Col sm={{ size: 8 }}>
-							      <Table>
-								      <thead>
-								      <tr>
-									      <th>#</th>
-									      <th>Poll Title</th>
-									      <th>View</th>
-									      <th>Remove</th>
-								      </tr>
-								      </thead>
-								      <tbody>
-								      <tr>
-									      <th scope="row">1</th>
-									      <td>Mark</td>
-									      <td>Otto</td>
-									      <td>@mdo</td>
-								      </tr>
-								      <tr>
-									      <th scope="row">2</th>
-									      <td>Jacob</td>
-									      <td>Thornton</td>
-									      <td>@fat</td>
-								      </tr>
-								      <tr>
-									      <th scope="row">3</th>
-									      <td>Larry</td>
-									      <td>the Bird</td>
-									      <td>@twitter</td>
-								      </tr>
-								      </tbody>
-							      </Table>
-						      </Col>
-					      </CardBody>
-				      </Card>
-			      </Col>
-		      </Row>
-	      </Container>
-      </div>
+    handleSubmit(e) {
+	    e.preventDefault();
+	    const votingPolls = localStorage.getItem("votingPolls") ? JSON.parse(localStorage.getItem("votingPolls")) : [];
+	    votingPolls.push(this.state);
+	    this.setState({
+		    votingPolls
+	    }, () => localStorage.setItem("votingPolls", JSON.stringify(votingPolls)));
+    }
+
+    render() {
+	  let listItems = [];
+	  const votingPolls = this.state.votingPolls;
+
+	  return (
+	    <div className="App">
+		    <Container>
+			    <Row>
+				    <Col className="p-3" sm="12">
+					    <h2>VOTING APP</h2>
+				    </Col>
+				    <Col sm={{size: 8, order: 2, offset: 1}}>
+					    <Card className="p-2">
+						    <CardBody className="text-left">
+							    <h3>Make a new POLL:</h3>
+							    <Col sm={{size: 8}}>
+								    <Form onSubmit={this.handleSubmit} ref="form">
+									    <FormGroup>
+										    <Label for="pollTitle">Title:</Label>
+										    <Input type="text" name="pollTitle" id="pollTitle"
+										           onChange={this.handleChange} placeholder="Poll title"/>
+									    </FormGroup>
+									    ​
+									    <FormGroup>
+										    <Label for="pollOptions">Options</Label>
+										    <Input type="select" name="pollOptions" id="pollOptions"
+										           onChange={this.handleChange} placeholder="Select">
+											    <option>Select Party</option>
+											    <option>Congress</option>
+											    <option>BJP</option>
+											    <option>Indian National Congress</option>
+											    <option>Nota</option>
+										    </Input>
+									    </FormGroup>
+									    <Button type="submit" color="primary">Create</Button>
+								    </Form>
+							    </Col>
+						    </CardBody>
+					    </Card>
+				    </Col>
+				    ​
+				    <Col className="mt-3" sm={{size: 8, order: 2, offset: 1}}>
+					    <Card className="p-2">
+						    <CardBody className="text-left">
+							    <h3>Polls List:</h3>
+							    <ul>{console.log(listItems)}</ul>
+							    <Col sm={{size: 8}}>
+								    <Table>
+									    <thead>
+									    <tr>
+										    <th>#</th>
+										    <th>Poll Title</th>
+										    <th>View</th>
+										    <th>Remove</th>
+									    </tr>
+									    </thead>
+									    <tbody>
+									    {votingPolls.map((votingPoll, key) => <tr key={key}>
+										    <td>{key + 1}</td>
+										    <td>
+											    {votingPoll.pollTitle}
+										    </td>
+										    <td>
+											    {votingPoll.pollOptions}
+										    </td>
+										    <td>
+											    Remove
+										    </td>
+									    </tr>)}
+									    </tbody>
+								    </Table>
+							    </Col>
+						    </CardBody>
+					    </Card>
+				    </Col>
+			    </Row>
+		    </Container>
+	    </div>
     );
   }
 }
